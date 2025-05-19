3847fea1122c4aee4b1783e1ba81f5e7ec8da2bf
Stefan Rimaila <stefan@rimaila.fi>
2025-05-20 00:40:39 +0300

    chore: remove post content

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,19 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-});
+// const blog = defineCollection({
+//   // Load Markdown and MDX files in the `src/content/blog/` directory.
+//   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+//   // Type-check frontmatter using a schema
+//   schema: z.object({
+//     title: z.string(),
+//     description: z.string(),
+//     // Transform string to Date object
+//     pubDate: z.coerce.date(),
+//     updatedDate: z.coerce.date().optional(),
+//     heroImage: z.string().optional(),
+//   }),
+// });
 
 const work = defineCollection({
   loader: glob({ base: './src/content/work', pattern: '**/*.{md,mdx}' }),
@@ -27,4 +27,4 @@ const work = defineCollection({
     }),
 });
 
-export const collections = { blog, work };
+export const collections = { work };
