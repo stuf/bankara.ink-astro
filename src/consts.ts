050a1df2cc8b324429805a77ee62bb4c0f2481fe
Stefan Rimaila <stefan@rimaila.fi>
2025-05-19 18:39:12 +0300

    feat: add everything

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,16 @@
 
 export const SITE_TITLE = 'bankara.ink';
 export const SITE_DESCRIPTION = 'A collection of virtual photographs';
+
+export const IMAGE_SIZE = {
+  sm: 240,
+  md: 480,
+  lg: 720,
+  xl: 1440,
+};
+
+const img = IMAGE_SIZE;
+
+export const IMAGE_WIDTHS = [240, 480, 720, 1440];
+export const WORK_GALLERY_IMAGE_WIDTHS = [img.sm, img.md, img.lg];
+export const WORK_DETAIL_IMAGE_WIDTHS = [img.md, img.lg, img.xl];
