ef508fa18f2f36cf550ed4b2c6fbca754f91b1b2
Stefan Rimaila <stefan@rimaila.fi>
2025-06-11 08:19:45 +0300

    feat: add posts to content config

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,6 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-// const blog = defineCollection({
-//   // Load Markdown and MDX files in the `src/content/blog/` directory.
-//   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-//   // Type-check frontmatter using a schema
-//   schema: z.object({
-//     title: z.string(),
-//     description: z.string(),
-//     // Transform string to Date object
-//     pubDate: z.coerce.date(),
-//     updatedDate: z.coerce.date().optional(),
-//     heroImage: z.string().optional(),
-//   }),
-// });
-
 const work = defineCollection({
   loader: glob({ base: './src/content/work', pattern: '**/*.{md,mdx}' }),
   schema: ({ image }) =>
@@ -31,4 +17,17 @@ const work = defineCollection({
     }),
 });
 
-export const collections = { work };
+const post = defineCollection({
+  loader: glob({ base: './src/content/post', pattern: '**/*.{md,mdx}' }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image(),
+      public: z.boolean().optional(),
+    }),
+});
+
+export const collections = { work, post };
