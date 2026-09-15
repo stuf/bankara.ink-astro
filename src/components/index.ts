1b00f93f5f9018765574fd69732b58a26633bd3b
Stefan Rimaila <stefan@rimaila.fi>
2026-09-15 11:11:23 +0300

    feat: dev mode, journal

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
 export { default as Header } from './Header.astro';
 export { default as Footer } from './Footer.astro';
 export { default as TOC } from './TOC.astro';
+export { default as TOCHeading } from './TOCHeading.astro';
+
+export { default as FormattedDate } from './FormattedDate.astro';
+export { default as Icon } from './Icon.astro';
+export { default as Tag } from './Tag.astro';
